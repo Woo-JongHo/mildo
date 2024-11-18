b25cdee3696e17b9c1f6ece8a700a1d03efb6bd5
Imdo714 <gusehdla@naver.com>
2024-11-18 19:22:37 +0900

    223

---
+++ src/main/java/com/swyp/mildo/MildoApplication.java
@@ -0,0 +1,13 @@
+package com.swyp.mildo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MildoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MildoApplication.class, args);
+	}
+
+}
