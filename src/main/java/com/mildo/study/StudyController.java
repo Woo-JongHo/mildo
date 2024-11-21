726fd0c30dccad6784882a4c276d3abdab9f7ebc
Seonghun Lee <dlfyd05680@gmail.com>
2024-11-21 14:21:28 +0900

    Study Mybatis 테스트중

--- src/main/java/com/mildo/study/StudyController.java
+++ src/main/java/com/mildo/study/StudyController.java
@@ -1,4 +1,30 @@
 package com.mildo.study;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/study")
 public class StudyController {
+    private final StudyService studyService;
+
+    @GetMapping
+    public String index() {
+        log.info("index");
+        return "Study Home";
+    }
+
+    @PostMapping("/create")
+    public String create(String name, String password) {
+        log.info("create Study name: {} and password: {}", name, password);
+        studyService.create(name, password);
+        return "Create Study";
+    }
+
 }
--- src/main/java/com/mildo/study/StudyDBManger.java
+++ src/main/java/com/mildo/study/StudyDBManger.java
@@ -1,4 +1,24 @@
 package com.mildo.study;
 
-public class StudyDBManger {
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.session.SqlSession;
+
+import com.mildo.db.DBManger;
+
+@Slf4j
+public class StudyDBManger extends DBManger {
+
+    public static int create(StudyVO s) {
+        log.info("StudyDBManger: study create {}", s);
+        int re = -1;
+        if (sqlSessionFactory == null) {
+            log.error("StudyDBManger: session is null");
+        } else {
+            SqlSession session = sqlSessionFactory.openSession();
+            re = session.insert("Study.create", s);
+            session.commit();
+            session.close();
+        }
+        return re;
+    }
 }
