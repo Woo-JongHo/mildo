e591013998801eebe2180717da16c2c42bdc09e4
Imdo714 <gusehdla@naver.com>
2024-11-24 20:24:46 +0900

    [FEAT] 스터디코드 API조회

--- src/main/java/com/mildo/study/StudyController.java
+++ src/main/java/com/mildo/study/StudyController.java
@@ -2,10 +2,14 @@ package com.mildo.study;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -27,5 +31,24 @@ public class StudyController {
 
         return "Create Study";
     }
+
+    // studyCode로 스터디 리스트 가져오기 | 호출 방법 /api/%23E3R4/memberList
+    @GetMapping("/api/{studyCode}/memberList")
+    public Map<String, Object> studyList(@PathVariable String studyCode) {
+        studyCode = URLDecoder.decode(studyCode, StandardCharsets.UTF_8);
+        log.info("studyCode = {}", studyCode);
+
+        List<StudyVO> list = studyService.studyList(studyCode);
+        int totalMembers = studyService.totalMembers(studyCode);
+        log.info("list = {}", list);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("members", list);
+        response.put("totalMembers", totalMembers);
+
+        return response;
+    }
+
+
 
 }
