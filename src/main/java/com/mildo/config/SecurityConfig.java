268f3651a21e5dff668fe9602a7361c2ce5006cf
Imdo714 <gusehdla@naver.com>
2024-12-02 20:55:43 +0900

    [FIX] 디버깅 추가

--- src/main/java/com/mildo/config/SecurityConfig.java
+++ src/main/java/com/mildo/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.mildo.config;
 
+import com.mildo.user.Auth.Auth2FailureHandler;
 import com.mildo.user.Auth.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,17 +14,16 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableWebSecurity
 public class SecurityConfig {
 
-    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-
-    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
-        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    }
+//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+//
+//    public SecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter) {
+//        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((auth) -> auth
-//                        .requestMatchers("/", "/login", "/google-login", "/google-login2").permitAll()
                         .requestMatchers("/", "/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/my/**").hasAnyRole("ADMIN", "USER")
@@ -33,6 +33,7 @@ public class SecurityConfig {
         http
                 .oauth2Login((oauth2) -> oauth2
                         .defaultSuccessUrl("/home", true) // 로그인 성공 시 이동할 URL
+                        .failureHandler(new Auth2FailureHandler())
                         .authorizationEndpoint(endpoint -> endpoint
                                 .authorizationRequestRepository(new HttpSessionOAuth2AuthorizationRequestRepository())
                         )
