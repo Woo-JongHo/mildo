0080ab1dbdfb6ecd6131a8df7320c78c3f7116a0
Imdo714 <gusehdla@naver.com>
2024-12-08 12:13:54 +0900

    [FIX] Refresh Token 검증 버그 수정

--- src/main/java/com/mildo/config/SecurityConfig.java
+++ src/main/java/com/mildo/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.mildo.config;
 
 import com.mildo.user.Auth.Auth2FailureHandler;
-import com.mildo.user.Auth.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
