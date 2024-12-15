12e227a2cdb48756a521435a2581785d49f2ef25
Woo-JonHo <jonghowoo33@gmail.com>
2024-12-15 23:05:45 +0900

    [FIX] 익스텐션 CORS 추가

--- src/main/java/com/mildo/config/WebConfig.java
+++ src/main/java/com/mildo/config/WebConfig.java
@@ -16,7 +16,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("https://school.programmers.co.kr", "https://www.podofarm.xyz", "https://podofarm.xyz",
-                        "http://mildo.xyz", "http://www.mildo.xyz", "https://mildo.xyz", "https://www.mildo.xyz",
+                        "http://mildo.xyz", "http://www.mildo.xyz", "https://mildo.xyz", "https://www.mildo.xyz", "https://api.mildo.xyz",
                         "http://localhost:5173", "http://localhost:5174", "http://localhost:5175","chrome-extension://kmleenknngfkjncchnbfenfamoighddf") // 허용할 도메인 설정
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
